f6cc7ba18ac12f6657932102f780d53cf45aaa40
Rafael Alves <rafaelealvesnascimento919@gmail.com>
2025-09-05 15:23:24 -0300

    Terminei

--- contatos/main.js
+++ contatos/main.js
@@ -1,18 +1,39 @@
-let user_name = String(document.querySelector("#name").value)
-let user_email = String(document.querySelector("#email").value)
-let labels = document.querySelector("#labels")
-let enviar = document.querySelector("#enviar")
+const labels = document.querySelector("#labels")
+const enviar = document.querySelector("#enviar")
+let adi = -1
 
 enviar.addEventListener("click", criar)
 
 let peoples = []
 function criar() {
+    const user_name = String(document.querySelector("#name").value)
+    const user_email = String(document.querySelector("#email").value)
+    const user_msg = String(document.querySelector("#msg_text").value)
+
     let people = {
         name: user_name,
-        email: user_email
+        email: user_email,
+        msg: user_msg
     }
+
+    adi++
 
     peoples.push(people)
 
-    labels.textContent = peoples[0]["name"]
+    const tr = document.createElement("tr")
+    const td_name = document.createElement("td")
+    const td_email = document.createElement("td")
+    const td_msg = document.createElement("td")
+
+    td_name.textContent = peoples[adi]["name"]
+    tr.appendChild(td_name)
+    labels.appendChild(tr)
+
+    td_email.textContent = peoples[adi]["email"]
+    tr.appendChild(td_email)
+    labels.appendChild(tr)
+
+    td_msg.textContent = peoples[adi]["msg"]
+    tr.appendChild(td_msg)
+    labels.appendChild(tr)
 }
